a58432a8cbfd1d76f12f5e640d2e1210a0421131
David  Hendler <dhendler@bellmore-merrick.k12.ny.us>
2021-08-26 12:35:38 -0400

    first commit

---
+++ app/src/main/java/com/example/addwithouttextview/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.addwithouttextview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        int num1  = 90;
+        int num2 = 9;
+        int num3 = 4;
+        int sum = num1 + num2 +num3;
+        System.out.println("sum" + sum);
+        Log.d("sum" , String.valueOf( sum ) );git
+    }
+}
